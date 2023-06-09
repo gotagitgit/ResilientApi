7bb167a3a49280cffb0162426931dfe63c53db84
Cyrus Penuliar <Cyrus.Penuliar@infor.com>
2023-06-09 16:19:51 +0800

    Use Resiliency exentions

--- src/Resilient.Api/Program.cs
+++ src/Resilient.Api/Program.cs
@@ -1,28 +1,24 @@
-using Polly;
 using Resilient.Api;
-using Resilient.Api.Factories;
+using Resilient.Api.Extensions;
 using Resilient.Api.Settings;
 using Web.Common;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 var services = builder.Services;
 
 services.Configure<TodoApiSetting>(builder.Configuration.GetSection(nameof(TodoApiSetting)));
 
 services.RegisterApiDependencies()
         .RegisterWebCommonDependencies()
-        .RegisterTodosHttpClient(builder.Configuration);
+        .AddResilientStrategies(builder.Configuration);
 
 services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 services.AddEndpointsApiExplorer();
 services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
---
+++ src/Resilient.Api/Settings/ResilienceSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Resilient.Api.Settings;
+
+public class ResilienceSettings
+{
+    public int RetryCount { get; set; }
+
+    public int ExceptionsAllowedBeforeBreaking { get; set; }
+}
